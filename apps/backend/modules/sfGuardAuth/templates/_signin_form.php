92ca92e4d40b21c69bd1854ec0b9b7ebe81ae22e
Alix Chaysinh <alixc@theodo.fr>
2013-11-22 17:27:31 +0100

    [master] Add routes and frontend link in backend

--- apps/backend/modules/sfGuardAuth/templates/_signin_form.php
+++ apps/backend/modules/sfGuardAuth/templates/_signin_form.php
@@ -18,6 +18,9 @@
           <?php if (isset($routes['sf_guard_register'])): ?>
             &nbsp; <a href="<?php echo url_for('@sf_guard_register') ?>"><?php echo __('Want to register?', null, 'sf_guard') ?></a>
           <?php endif; ?>
+
+
+            &nbsp; <a href="http://<?php echo $sf_request->getHost() ?>/index.php"><?php echo __('Want to register?', null, 'sf_guard') ?></a>
         </td>
       </tr>
     </tfoot>
