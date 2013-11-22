900314fc763ac98267259d1f91303d04b5b42d7c
Alix Chaysinh <alixc@theodo.fr>
2013-11-22 11:14:41 +0100

    [master] Generate sf project

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    G-test-project
+ * @subpackage form
+ * @author     alix.chaysinh@gmail.com 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
