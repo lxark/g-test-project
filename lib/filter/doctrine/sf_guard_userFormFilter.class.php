4edf74f482952b47dabb67199e1b6e790ce36743
Alix Chaysinh <alixc@theodo.fr>
2013-11-22 15:48:37 +0100

    [master] Oops forgot generated model classes

---
+++ lib/filter/doctrine/sf_guard_userFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sf_guard_user filter form.
+ *
+ * @package    G-test-project
+ * @subpackage filter
+ * @author     alix.chaysinh@gmail.com
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sf_guard_userFormFilter extends Basesf_guard_userFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/sf_guard_userForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sf_guard_user form.
+ *
+ * @package    G-test-project
+ * @subpackage form
+ * @author     alix.chaysinh@gmail.com
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sf_guard_userForm extends Basesf_guard_userForm
+{
+  public function configure()
+  {
+  }
+}
